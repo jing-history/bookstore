e42123b1ff70fa9232544e745d5ae9c5a2bcfca7
wangyj <wangyj@soonfor.com>
2018-11-07 18:28:15 +0800

    :tada: bookstore vue init

---
+++ src/Me.vue
@@ -0,0 +1,19 @@
+<style lang="less">
+</style>
+
+<template>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  };
+</script>
